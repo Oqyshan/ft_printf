c7506fcab597b97a8758b819243954aff1fa69db
Oqyshan <123980594+Oqyshan@users.noreply.github.com>
2023-07-23 15:36:56 +0300

    ft_printf yaptık.

---
+++ ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ounal <ounal@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/23 15:02:18 by ounal             #+#    #+#             */
+/*   Updated: 2023/07/23 15:34:22 by ounal            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *str, ...);
+int	ft_format(va_list *args, char c);
+int	ft_int(int a);
+int	ft_hex(unsigned int a, char c);
+int	ft_point(unsigned long a, int sign);
+int	ft_string(char *str);
+int	ft_unsigned(unsigned int a);
+
+#endif
